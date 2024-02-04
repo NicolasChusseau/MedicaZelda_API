3977a74fd2c9ea4fa6784ad8d66f59edcc18f4cf
NicolasChusseau <nchusseau@free.fr>
2024-02-04 13:48:50 +0100

    Adding labels to joiObjects

--- joiObjects.mjs
+++ joiObjects.mjs
@@ -6,7 +6,8 @@ export const medecinGouv = Joi.object({
     lastname: Joi.string().required().description('The lastname of the practitioner'),
     email: Joi.string().required().description('Email of the practitioner'),
     gender: Joi.string().required().description('The gender of the practitioner'),
-}).description('Information about a practitioner using GOUV API');
+}).description('Information about a practitioner using GOUV API')
+    .label('MedecinGouv');
 
 export const medecinInstamed = Joi.object({
     rpps: Joi.string().required().description('RPPS number of the practitioner'),
@@ -17,9 +18,12 @@ export const medecinInstamed = Joi.object({
     address: Joi.string().required().description('Address of the practitioner'),
     zipCode: Joi.string().required().description('Zip code of the practitioner'),
     city: Joi.string().required().description('City of the practitioner'),
-}).description('Information about a practitioner using INSTAMED API');
+}).description('Information about a practitioner using INSTAMED API')
+    .label('MedecinInstamed');
 
-export const arrayMedecinInstamed = Joi.array().items(medecinInstamed).description('Array of practitioners');
+export const arrayMedecinInstamed = Joi.array().items(medecinInstamed)
+    .description('Array of practitioners')
+    .label('ArrayMedecinInstamed');
 
 export const medecin = Joi.object({
     rpps: Joi.string().required().description('RPPS number of the practitioner'),
@@ -31,13 +35,17 @@ export const medecin = Joi.object({
     zipCode: Joi.string().required().description('Zip code of the practitioner'),
     city: Joi.string().required().description('City of the practitioner'),
     gender: Joi.string().required().description('The gender of the practitioner'),
-}).description('Information about a practitioner using both GOUV and INSTAMED API');
+}).description('Information about a practitioner using both GOUV and INSTAMED API')
+    .label('Medecin');
 
-export const arrayMedecin = Joi.array().items(medecin).description('Array of practitioners');
+export const arrayMedecin = Joi.array().items(medecin)
+    .description('Array of practitioners')
+    .label('ArrayMedecin');
 
 export const error404 = Joi.object({
     message: Joi.string().description('Error message'),
-}).description('Error 404');
+}).description('Error 404')
+    .label('Error404');
 
 export const rpps = Joi.object({
     rpps: Joi.string().required().description('RPPS number of the practitioner'),
